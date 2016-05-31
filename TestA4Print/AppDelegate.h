bb599eda7fc057b406daaac597d6ba6a5b0ff6e5
Diogo Carneiro <contato@idiogo.com.br>
2016-05-30 21:47:12 -0300

    Initial Commit

---
+++ TestA4Print/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestA4Print
+//
+//  Created by Diogo Carneiro on 30/05/16.
+//  Copyright © 2016 AppNinja. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestA4Print/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestA4Print
+//
+//  Created by Diogo Carneiro on 30/05/16.
+//  Copyright © 2016 AppNinja. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
